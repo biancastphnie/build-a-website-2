6d8120c87abeea19921c0aa681aad8b3a18829df
biancastphnie <biancastphnie@gmail.com>
2020-12-31 00:06:40 +0700

    edited city

--- src/components/City.js
+++ src/components/City.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-// props -> cityName, temp, color
+// props -> cityName ,temp, color
 function City({ cityName, temp, color }) {
+  const history = useHistory();
+
+  function handleClick() {
+    history.push("/city?name=" + cityName);
+  }
+
   return (
     <button
-      href={"?/city?name" + cityName}
       className={"flex flex-row p-8 justify-between items-center " + color}
+      onClick={handleClick}
     >
-      <div className="text-2xl">{cityName}</div>
-      <div className="text-4xl">{temp}</div>
+      <div className="text-2xl font-light">{cityName}</div>
+      <div className="text-5xl">{temp}</div>
     </button>
   );
 }
