6d8392532b41e6af479380639ad4cac85ca6aa60
biancastphnie <biancastphnie@gmail.com>
2021-01-01 12:40:17 +0700

    01/01/2021 progress

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -1,27 +1,95 @@
 import React, { useState, useEffect, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import WeatherImage from "../components/WeatherImage";
+import "../App.css";
 import dinodiscord from "../assets/dinodiscord.png";
 
 function Details() {
-  // TODO
+  const history = useHistory();
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_WEATHER_KEY);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        setWeatherData(weather);
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.log(error);
+      });
+  }, [city]);
+
+  useEffect(() => {
+    const searchParams = history.location.search;
+    const urlParams = new URLSearchParams(searchParams);
+    const city = urlParams.get("name");
+    if (city) {
+      setCity(city);
+    }
+  }, [history]);
+
+  const {
+    cloudiness,
+    currentTemp,
+    highTemp,
+    humidity,
+    lowTemp,
+    weatherType,
+    windSpeed,
+  } = useMemo(() => {
+    let cloudiness = "";
+    let currentTemp = "";
+    let highTemp = "";
+    let humidity = "";
+    let lowTemp = "";
+    let weatherType = "";
+    let windSpeed = "";
+
+    if (weatherData) {
+      cloudiness = `${weatherData.clouds.all}%`;
+      currentTemp = `${Math.round(weatherData.main.temp)}°C`;
+      highTemp = `${Math.round(weatherData.main.temp_max)}°C`;
+      humidity = `${weatherData.main.humidity}%`;
+      lowTemp = `${Math.round(weatherData.main.temp_min)}°C`;
+      weatherType = `${weatherData.weather[0].description}`;
+      windSpeed = `${weatherData.wind.speed} mph`;
+    }
+
+    return {
+      cloudiness,
+      currentTemp,
+      highTemp,
+      humidity,
+      lowTemp,
+      weatherType,
+      windSpeed,
+    };
+  }, [weatherData]);
+
   return (
     // Container
-    <div className="flex flex-col items-center h-screen bg-blue-200 font">
-      <div className="p-8 text-2xl font-semibold font-serif">
-        Weather in CityName
+    <div className="flex flex-col items-center h-screen bg-blue-200">
+      <div className="pt-8 pl-8 pr-8 text-2xl font-bold">Weather in {city}</div>
+
+      <div className="flex flex-col p-8 m-4 border-2 rounded-md border-gray-700 items-center">
+        <WeatherImage weatherType={weatherType} className="text-xl" />
+        <div>{weatherType}</div>
+        <div>current temperature : {currentTemp}</div>
       </div>
 
-      <div className="flex flex-col p-8 border-2 rounded-md border-gray-500 items center">
-        <div>haze</div>
-        <div>current temperature</div>
-      </div>
-
-      <div>highest temperature</div>
-      <div>cloudiness</div>
-      <div>lowest temperature</div>
-      <div>humidity</div>
-      <div>wind speed</div>
-      <div>chance of rain</div>
-
+      <div>high temperature : {highTemp}</div>
+      <div>cloudiness : {cloudiness}</div>
+      <div>low temperature : {lowTemp}</div>
+      <div>humidity : {humidity}</div>
+      <div>wind speed : {windSpeed}</div>
       <img src={dinodiscord} width="20%" height="20%" />
     </div>
   );
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,23 +1,102 @@
-import React, { useState, useEffect, useMemo } from "react";
-// import axios from "axios";
-// import { useHistory } from "react-router-dom";
-
-import Header from "../components/Header";
-import WeatherImage from "../components/WeatherImage";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import City from "../components/City";
 
-const weatherKey = `TODO`; // Your API Key here
-
 function Home() {
-  // TODO
+  const [cities, setCities] = useState([
+    {
+      name: "Jakarta",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-purple-100",
+    },
+    {
+      name: "Seattle",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-purple-200",
+    },
+    {
+      name: "Tokyo",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-purple-300",
+    },
+    {
+      name: "Seoul",
+      currentTemp: "0",
+      weatherType: "",
+      color: "bg-purple-400",
+    },
+    // {
+    //   name: "New York",
+    //   currentTemp: "0",
+    //   weatherType: "",
+    //   color: "bg-blue-500",
+    // },
+    // {
+    //   name: "Tokyo",
+    //   currentTemp: "0",
+    //   weatherType: "",
+    //   color: "bg-blue-500",
+    // },
+  ]);
+
+  useEffect(() => {
+    updateAllWeatherData();
+  }, []);
+
+  // Fetch the weather data for 1 city
+  async function fetchWeatherData(cityName) {
+    const res = await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        return weather;
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.warn(error);
+      });
+
+    return res;
+  }
+
+  // update the list data
+  async function updateAllWeatherData(params) {
+    cities.forEach(function (citiesItems, index) {
+      let weatherData = {};
+      let newCities = [...cities];
+
+      fetchWeatherData(citiesItems.name).then((res) => {
+        weatherData = res;
+
+        newCities[index].currentTemp = weatherData.main.temp;
+        newCities[index].weatherType = weatherData.weather[0].main;
+        setCities(newCities);
+      });
+    });
+  }
+
   return (
     // Container
-    <div className="flex flex-col h-screen bg-blue-200 text-4x">
-      <City cityName={"Jakarta"} temp={"30°C"} color={"bg-purple-400"} />
-      <City cityName={"Seattle"} temp={"5°C"} color={"bg-purple-200"} />
-      <City cityName={"Tokyo"} temp={"12°C"} color={"bg-purple-300"} />
-      <City cityName={"Seoul"} temp={"-9°C"} color={"bg-purple-100"} />
+    <div className="flex flex-col h-screen bg-white">
+      <div className="text-4xl text-purple-800 font-semibold text-center m-6">
+        Dnusrr's Weather App
+      </div>
+
+      {cities.map((item, index) => (
+        <City
+          cityName={item.name}
+          weatherType={item.weatherType}
+          temp={item.currentTemp}
+          color={item.color}
+        />
+      ))}
     </div>
   );
 }
